2d4bbdba393ed253a4682012da1d1941f1fdc81d
alexandra <83598143+gorillagripcore@users.noreply.github.com>
2023-11-07 21:38:14 +0100

    Finished 🍒

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 
-TOKEN = 'your token'
+TOKEN = 'MTE3MTEyNTIwOTU4NDExMTc1OA.GLwHaM.MKI_mf_eBvyLkbrzr9Tqd4DdcnBTCJo7tr-b6k'
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -23,7 +23,6 @@ async def on_ready():
 async def on_message(message):
     global voting_ready, books_received, book_messages, voting_month
 
-    #makes sure the bot only responds to this in the channel "vote-input"
     if message.author == client.user or message.channel.name != "vote-input":
         return
 
@@ -36,62 +35,83 @@ async def on_message(message):
         return
 
     if message.channel.name == "vote-input" and voting_ready:
-        if books_received < 2:
-            book_messages.append(message)
-            books_received += 1
-            if books_received == 1:
-                await message.channel.send("First book received. Please send the second book.")
-            elif books_received == 2:
-                voting_month = await ask_for_month(message)
-                await message.channel.send(f"Month for voting: {voting_month}.")
-                emojis = await ask_for_emojis(message)
-                if emojis:
-                    await process_books(emojis, voting_month)
-                    books_received = 0
-                    voting_ready = False
+        try:
+            if books_received < 2:
+                book_messages.append(message)
+                books_received += 1
+                if books_received == 1:
+                    await message.channel.send("First book received. Please send the second book.")
+                elif books_received == 2:
+                    voting_month = await ask_for_month(message)
+                    emojis = await ask_for_emojis(message)
+                    if emojis:
+                        await process_books(emojis, voting_month)
+                        books_received = 0
+                        voting_ready = False
+        except Exception:
+            await message.channel.send(f"An error occurred while processing the vote")
+            voting_ready = False
+            books_received = 0
+            book_messages = []
 
 async def process_books(emojis, month):
-    first_book = book_messages[0]
-    second_book = book_messages[1]
+    try:
+        first_book = book_messages[0]
+        second_book = book_messages[1]
 
-    first_book_title = first_book.content.split(",")[0].strip()
-    second_book_title = second_book.content.split(",")[0].strip()
+        embed1 = create_book_embed(first_book)
+        embed2 = create_book_embed(second_book)
+        if not embed1 or not embed2:
+            await first_book.channel.send("Error: One of the books weren't formatted correctly. Please start over.")
+            return
 
-    embed1 = create_book_embed(first_book)
-    embed2 = create_book_embed(second_book)
+        first_book_title = first_book.content.split(",")[0].strip()
+        second_book_title = second_book.content.split(",")[0].strip()
 
-    vote_message = f"Voting for {month}! React with {emojis[0]} for '{first_book_title}' and {emojis[1]} for '{second_book_title}'"
+        vote_message = f"Voting for {month}! React with {emojis[0]} for '{first_book_title}' and {emojis[1]} for '{second_book_title}'"
 
-    voting_channel = discord.utils.get(first_book.guild.channels, name="✨-voting-✨")
-    if voting_channel:
-        message_to_react = await voting_channel.send(content=vote_message, embeds=[embed1, embed2])
-        await message_to_react.add_reaction(emojis[0])
-        await message_to_react.add_reaction(emojis[1])
+        voting_channel = discord.utils.get(first_book.guild.channels, name="✨-voting-✨")
+        if voting_channel:
+            message_to_react = await voting_channel.send(content=vote_message, embeds=[embed1, embed2])
+            await message_to_react.add_reaction(emojis[0])
+            await message_to_react.add_reaction(emojis[1])
+        else:
+            await first_book.channel.send("Error: Voting channel not found. Please check if the channel '✨-voting-✨' exists.")
+    except discord.DiscordException:
+        await first_book.channel.send(f"A Discord error occurred")
 
 def create_book_embed(message):
-    image_url = message.attachments[0].url
-    parts = message.content.split(",")
-    book_title = parts[0].strip()
-    author = parts[1].strip()
-    embed = discord.Embed(title=book_title, description=f"by {author}")
-    embed.set_image(url=image_url)
-    return embed
+    try:
+        image_url = message.attachments[0].url
+        parts = message.content.split(",")
+        book_title = parts[0].strip()
+        author = parts[1].strip()
+        embed = discord.Embed(title=book_title, description=f"by {author}")
+        embed.set_image(url=image_url)
+        return embed
+    except IndexError:
+        return None
 
 async def ask_for_emojis(message):
-    def check(reaction_message):
-        return reaction_message.author == message.author and reaction_message.channel == message.channel
-
-    await message.channel.send("Please enter the emojis you want to use for voting. Separate them with spaces.")
-    reaction_message = await client.wait_for('message', check=check)
-    return reaction_message.content.split()
+    try:
+        await message.channel.send("Please enter the emojis you want to use for voting. Separate them with spaces.")
+        reaction_message = await client.wait_for('message', check=lambda m: m.author == message.author and m.channel == message.channel)
+        emojis = reaction_message.content.split()
+        if len(emojis) != 2:
+            await message.channel.send("Error: You need to provide exactly two emojis for the vote. Please start over")
+            return None
+        return emojis
+    except Exception:
+        await message.channel.send(f"An error occurred while collecting emojis")
+        return None
 
 async def ask_for_month(message):
-    await message.channel.send("Please enter the month for the voting.")
-
-    def check(m):
-        return m.author == message.author and m.channel == message.channel
-
-    month_message = await client.wait_for('message', check=check)
-    return month_message.content.strip()
+    try:
+        await message.channel.send("Please enter the month for the voting.")
+        month_message = await client.wait_for('message', check=lambda m: m.author == message.author and m.channel == message.channel)
+        return month_message.content.strip()
+    except Exception:
+        await message.channel.send(f"An error occurred while setting the month")
+        return None
 
 client.run(TOKEN)
